33df2dcc1f4370d16bf0adb603d46ed23ffdd90a
Checan Nicholas C <ChecanNicholasC@JohnDeere.com>
2020-12-02 18:33:40 -0300

    Initial Commit :rocket:

---
+++ hello.py
@@ -0,0 +1,32 @@
+from flask import Flask
+import xlsxwriter
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    # Create an new Excel file and add a worksheet.
+    workbook = xlsxwriter.Workbook('demo.xlsx')
+    worksheet = workbook.add_worksheet()
+
+    # Widen the first column to make the text clearer.
+    worksheet.set_column('A:A', 20)
+
+    # Add a bold format to use to highlight cells.
+    bold = workbook.add_format({'bold': True})
+
+    # Write some simple text.
+    worksheet.write('A1', 'Hello')
+
+    # Text with formatting.
+    worksheet.write('A2', 'World', bold)
+
+    # Write some numbers, with row/column notation.
+    worksheet.write(2, 0, 123)
+    worksheet.write(3, 0, 123.456)
+
+    # Insert an image.
+    # worksheet.insert_image('B5', 'logo.png')
+
+    workbook.close()
+
+    return 'The spreadsheet was created.'
